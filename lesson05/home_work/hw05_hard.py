069a62dcd2cb146bd8ae42c6b301091f42c56aa5
mizrael63 <41732636+mizrael63@users.noreply.github.com>
2018-08-09 02:15:16 +0500

    Update hw05_hard.py

--- lesson05/home_work/hw05_hard.py
+++ lesson05/home_work/hw05_hard.py
@@ -12,4 +12,82 @@
 # Важно! Все операции должны выполняться в той директории, в который вы находитесь.
 # Исходной директорией считать ту, в которой был запущен скрипт.
 
-# P.S. По возможности, сделайте кросс-платформенную реализацию.
+import sys
+import shutil
+print('sys.argv = ', sys.argv)
+def print_help():
+    print("help - получение справки")
+    print("mkdir <dir_name> - создание директории")
+    print("ping - тестовый ключ")
+    print('cp <file_name> - создает копию указанного файла')
+    print('rm <file_name> - удаляет указанный файл')
+    print('cd <full_path or relative_path> - меняет текущую директорию на указанную')
+    print('ls - отображение полного пути текущей директории')
+def make_dir():
+    if not dir_name:
+        print("Необходимо указать имя директории вторым параметром")
+        return
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.mkdir(dir_path)
+        print('директория {} создана'.format(dir_name))
+    except FileExistsError:
+        print('директория {} уже существует'.format(dir_name))
+def copy_file():
+    if not dir_name:
+        print("Необходимо указать имя файла вторым параметром")
+        return
+    file_path = os.path.join(os.getcwd(), dir_name)
+    shutil.copy(file_path, file_path + ' - copy')
+    print('файл {} скопирован'.format(dir_name))
+def remove_file():
+    if not dir_name:
+        print("Необходимо указать имя файла вторым параметром")
+        return
+    answer = input('Вы действительно хотите удалить файл {}? (Д/Н) '.format(dir_name))
+    if answer.upper() != 'Д':
+        print('Отмена')
+        return
+    file_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.remove(file_path)
+        print('файл {} удалён'.format(dir_name))
+    except FileNotFoundError:
+        print('файл {} не существует'.format(dir_name))
+def change_dir():
+    if not dir_name:
+        print("Необходимо указать имя файла вторым параметром")
+        return
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.chdir(dir_path)
+        print('перешли в {}'.format(dir_name))
+    except FileNotFoundError:
+        print('папки {} не существует'.format(dir_name))
+def print_path():
+    print(os.getcwd())
+def ping():
+    print("pong")
+do = {
+    "help": print_help,
+    "mkdir": make_dir,
+    "ping": ping,
+    'cp': copy_file,
+    'rm': remove_file,
+    'cd': change_dir,
+    'ls': print_path
+}
+try:
+    dir_name = sys.argv[2]
+except IndexError:
+    dir_name = None
+try:
+    key = sys.argv[1]
+except IndexError:
+    key = None
+if key:
+    if do.get(key):
+        do[key]()
+    else:
+        print("Задан неверный ключ")
+        print("Укажите ключ help для получения справки")
